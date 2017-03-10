5d49604944747638c6e6d473cab9dc7727573522
ofrimarx <ofrimarx@ofri>
2017-03-10 22:57:30 +0200

    Added movement and another player

--- spaceShooter/src/spaceShooter/Main_Panel.java
+++ spaceShooter/src/spaceShooter/Main_Panel.java
@@ -2,27 +2,70 @@ package spaceShooter;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public class Main_Panel extends JPanel{
 
-	private Player p1;
+	private Player p1, p2;
+	private Timer gameTimer;
 	
 	public Main_Panel(Main_Frame frame)
-	{
+	{	
 		setBackground(Color.BLACK);
 		
-		p1 = new Player(50, frame.getHeight()-130);
+		frame.addKeyListener(new KeyAction());
+		
+		p1 = new Player(50, frame.getHeight()-130, Color.BLUE);
+		p2 = new Player(50, 70, Color.red);
+		
+		gameTimer = new Timer(5, new TimerAction());
+		gameTimer.start();
 	}
 	
 	@Override
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		
 		p1.paintPlayer(g);
+		p2.paintPlayer(g);
 	}
 	
+	
+	class KeyAction implements KeyListener{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			p1.pressed(e.getKeyCode());
+			p2.pressed(e.getKeyCode());
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			p1.released();
+			p2.released();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			//unused
+		}
+	}
+	
+	class TimerAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			p1.move();
+			p2.move();
+			repaint();
+		}
+		
+	}
 }
--- spaceShooter/src/spaceShooter/Player.java
+++ spaceShooter/src/spaceShooter/Player.java
@@ -1,19 +1,24 @@
 package spaceShooter;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 import java.awt.geom.Rectangle2D;
-import java.awt.*;
 
 public class Player {
 
-	private int x, y;
 	private final int SIZE = 6;
 	
-	public Player(int x, int y)
+	private int x, y;
+	private char direction = ' ';
+	private Color color;
+	
+	public Player(int x, int y, Color c)
 	{
 		this.x = x;
 		this.y = y;
+		this.color = c;
 	}
 	
 	public void paintPlayer(Graphics g)
@@ -22,7 +27,38 @@ public class Player {
 		
 		Rectangle2D body = new Rectangle2D.Double(x, y, 10*SIZE, 10*SIZE);
 		
-		g2d.setColor(Color.BLUE);
+		g2d.setColor(color);
 		g2d.fill(body);
+	}
+	
+	public void pressed(int key)
+	{
+		switch(key)
+		{
+		case KeyEvent.VK_LEFT:
+			direction = 'l';
+			break;
+		case KeyEvent.VK_RIGHT:
+			direction = 'r';
+			break;
+		}
+	}
+	
+	public void released()
+	{
+		direction = ' ';
+	}
+	
+	public void move()
+	{
+		switch(direction)
+		{
+		case 'l':
+			x -= 2;
+			break;
+		case 'r':
+			x += 2;
+			break;
+		}
 	}
 }
