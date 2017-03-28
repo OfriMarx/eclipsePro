b845c4a5d4ed91688737341b7777ae31266e598b
325174183 <325174183@Library2.ALE.LOCAL>
2017-03-28 14:01:37 +0300

    Swag lol

--- projects/src/curveFever/CF.java
+++ projects/src/curveFever/CF.java
@@ -1,27 +1,25 @@
-package snake;
+package curveFever;
 
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
-public class Snake_Frame extends JFrame{
+public class CF extends JFrame{
 
-	public Snake_Frame()
+	CF()
 	{
-		setTitle("Curvz");						
+		setTitle("Curvez");						
 		setResizable(false);					
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(605,600);					
+		setSize(600,600);					
 		setLocationRelativeTo(null);
 		setVisible(true);			
-		
-		Snake_Panel panel = new Snake_Panel(this);
-		this.add(panel);	
-		
+				
 		validate();
 	}
 	
 	public static void main(String[] args) {
-		new Snake_Frame();
+		new CF();
+
 	}
 
 }
