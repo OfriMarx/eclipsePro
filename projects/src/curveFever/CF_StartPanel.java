739263083c1469de24f64fbac1ab34f6a1b3f4b8
ofrimarx <ofrimarx@ofri>
2017-04-12 13:38:36 +0300

    added numofp option

--- projects/src/curveFever/CF_StartPanel.java
+++ projects/src/curveFever/CF_StartPanel.java
@@ -1,6 +1,7 @@
 package curveFever;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -16,12 +17,21 @@ import javax.swing.JPanel;
 public class CF_StartPanel extends JPanel{
 
 	private JButton selectedButton = null;
+	private int num = 2;
 	
 	public CF_StartPanel(CF frame)
 	{
 		setBackground(Color.black);
-		setLayout(new GridLayout(3, 3));
-		JPanel panel = this;
+		JPanel mainPanel = this;
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.black);
+		panel.setLayout(new GridLayout(num+1, 3));
+		panel.setPreferredSize(new Dimension(frame.getWidth(), 700));
+		
+		JPanel head = new JPanel();
+		head.setBackground(Color.BLACK);
+		head.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight()-panel.getPreferredSize().height));
 		
 		JLabel p1 = new JLabel("p1", JLabel.CENTER);
 		p1.setForeground(Color.red);
@@ -34,94 +44,22 @@ public class CF_StartPanel extends JPanel{
 		JButton p1l = new JButton(); 
 		p1l.setForeground(Color.red);
 		p1l.setFont(new Font("a", Font.BOLD, 30));
-		p1l.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(selectedButton != p1l)
-				{
-					if(selectedButton != null)
-						selectedButton.setBackground(Color.black);
-					
-					selectedButton = p1l;
-					p1l.setBackground(Color.RED);
-				}
-				else
-				{
-					selectedButton = null;
-					p1l.setBackground(Color.BLACK);
-				}
-				getTopLevelAncestor().requestFocus();
-			}
-		});
+		p1l.addActionListener(new ButtonAction(p1l));
 		
 		JButton p1r = new JButton(); 
 		p1r.setForeground(Color.red);
 		p1r.setFont(new Font("a", Font.BOLD, 30));
-		p1r.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(selectedButton != p1r)
-				{
-					if(selectedButton != null)
-						selectedButton.setBackground(Color.black);
-					
-					selectedButton = p1r;
-					p1r.setBackground(Color.red);
-				}
-				else
-				{
-					selectedButton = null;
-					p1r.setBackground(Color.BLACK);
-				}
-				getTopLevelAncestor().requestFocus();
-			}
-		});
+		p1r.addActionListener(new ButtonAction(p1r));
 		
 		JButton p2l = new JButton(); 
 		p2l.setForeground(Color.blue);
 		p2l.setFont(new Font("a", Font.BOLD, 30));
-		p2l.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(selectedButton != p2l)
-				{
-					if(selectedButton != null)
-						selectedButton.setBackground(Color.black);
-					
-					selectedButton = p2l;
-					p2l.setBackground(Color.blue);
-				}
-				else
-				{
-					selectedButton = null;
-					p2l.setBackground(Color.BLACK);
-				}
-				getTopLevelAncestor().requestFocus();
-			}
-		});
+		p2l.addActionListener(new ButtonAction(p2l));
 		
 		JButton p2r = new JButton();
 		p2r.setForeground(Color.blue);
 		p2r.setFont(new Font("a", Font.BOLD, 30));
-		p2r.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(selectedButton != p2r)
-				{
-					if(selectedButton != null)
-						selectedButton.setBackground(Color.black);
-					
-					selectedButton = p2r;
-					p2r.setBackground(Color.blue);
-				}
-				else
-				{
-					selectedButton = null;
-					p2r.setBackground(Color.BLACK);
-				}
-				getTopLevelAncestor().requestFocus();
-			}
-		});
+		p2r.addActionListener(new ButtonAction(p2r));
 		
 		JButton start = new JButton("start");
 		start.setBackground(Color.yellow);
@@ -129,21 +67,78 @@ public class CF_StartPanel extends JPanel{
 		start.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				frame.remove(panel);
+				frame.remove(mainPanel);
 				frame.add(new CF_Panel(frame, p1l.getText(), p1r.getText(), p2l.getText(), p2r.getText()));
 				frame.validate();
 			}
 		});
 		
+		JLabel numOfPlayers = new JLabel(Integer.toString(num));
+		numOfPlayers.setForeground(Color.GREEN);
+		numOfPlayers.setFont(new Font("a", Font.BOLD, 30));
 		
-		add(p1);
-		add(p1l);
-		add(p1r);
-		add(p2);
-		add(p2l);
-		add(p2r);
-		add(blank);
-		add(start);
+		JButton add = new JButton("+1");
+		add.setBackground(Color.GREEN);
+		add.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(num < 2)
+					num++;
+				numOfPlayers.setText(Integer.toString(num));
+				panel.setLayout(new GridLayout(num+1, 3));
+				panel.removeAll();
+				panel.add(p1);
+				panel.add(p1l);
+				panel.add(p1r);
+				if(num>1)
+				{
+					panel.add(p2);
+					panel.add(p2l);
+					panel.add(p2r);
+				}
+				panel.add(blank);
+				panel.add(start);
+			}
+		});
+		
+		JButton decrease = new JButton("-1");
+		decrease.setBackground(Color.GREEN);
+		decrease.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(num > 1)
+					num--;
+				numOfPlayers.setText(Integer.toString(num));
+				panel.setLayout(new GridLayout(num+1, 3));
+				panel.removeAll();
+				panel.add(p1);
+				panel.add(p1l);
+				panel.add(p1r);
+				if(num>1)
+				{
+					panel.add(p2);
+					panel.add(p2l);
+					panel.add(p2r);
+				}
+				panel.add(blank);
+				panel.add(start);
+			}
+		});
+		
+		panel.add(p1);
+		panel.add(p1l);
+		panel.add(p1r);
+		panel.add(p2);
+		panel.add(p2l);
+		panel.add(p2r);
+		panel.add(blank);
+		panel.add(start);
+		head.add(add);
+		head.add(numOfPlayers);
+		head.add(decrease);
+		add(head);
+		add(panel);
+		
 		frame.addKeyListener(new KeyAction());
 	}
 
@@ -171,5 +166,32 @@ public class CF_StartPanel extends JPanel{
 			
 		}
 		
+	}
+	
+	class ButtonAction implements ActionListener
+	{
+		private JButton b;
+		public ButtonAction(JButton b)
+		{
+			this.b = b;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(selectedButton != b)
+			{
+				if(selectedButton != null)
+					selectedButton.setBackground(Color.black);
+				
+				selectedButton = b;
+				b.setBackground(Color.green);
+			}
+			else
+			{
+				selectedButton = null;
+				b.setBackground(Color.BLACK);
+			}
+			getTopLevelAncestor().requestFocus();
+		}
 	}
 }
