c62733ea13f1294f291b9633a3a3eeec05e5e512
325174183 <325174183@R34S04.ALE.LOCAL>
2017-11-23 09:40:32 +0200

    fixed and added randomize starting points

--- projects/src/curveFever/CF_Panel.java
+++ projects/src/curveFever/CF_Panel.java
@@ -3,6 +3,7 @@ package curveFever;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -26,41 +27,68 @@ public class CF_Panel extends JPanel{
 	private CF frame;
 	private int alive = 0;
 	
-	public CF_Panel(CF frame, String left1, String right1, String left2, String right2, String left3, String right3, String left4, String right4, int num)
+	public CF_Panel(CF frame, String left1, String right1, String left2, String right2, String left3, String right3, String left4, String right4, int numOfPlayers)
 	{
 		setBackground(Color.BLACK);
 		this.frame = frame;
 		
-		p1 = new CurvePlayer(15,60, Color.red, right1, left1, 0);
+		int frameHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+		int frameWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+		
+		//Randomizing starting points and angles
+		int xStart = (int)(Math.random()*(frameWidth-2));
+		int yStart = (int)(Math.random()*(frameHeight-2));
+		int angle = (int)(Math.random()*360);
+		
+		p1 = new CurvePlayer(xStart, yStart, Color.red, right1, left1, angle);
 		setBindings(p1, "p1");
 		alive ++;
+		playerList.add(p1);
 		
-		p2 = new CurvePlayer(15,120, Color.blue, right2, left2, 0);
+		xStart = (int)(Math.random()*(frameWidth-2));
+		yStart = (int)(Math.random()*(frameHeight-2));
+		angle = (int)(Math.random()*360);
+		
+		p2 = new CurvePlayer(xStart, yStart, Color.blue, right2, left2, angle);
 		setBindings(p2, "p2");
 		alive ++;
+		playerList.add(p2);
 		
-		p3 = new CurvePlayer(15,300, Color.green, right3, left3, 0);
+		xStart = (int)(Math.random()*(frameWidth-2));
+		yStart = (int)(Math.random()*(frameHeight-2));
+		angle = (int)(Math.random()*360);
+		
+		p3 = new CurvePlayer(xStart, yStart, Color.green, right3, left3, angle);
 		setBindings(p3, "p3");
 		alive ++;
+		playerList.add(p3);
 		
-		p4 = new CurvePlayer(15,500, Color.cyan, right4, left4, 0);
+		xStart = (int)(Math.random()*(frameWidth-2));
+		yStart = (int)(Math.random()*(frameHeight-2));
+		angle = (int)(Math.random()*360);
+		
+		p4 = new CurvePlayer(xStart, yStart, Color.cyan, right4, left4, angle);
 		setBindings(p4, "p4");
 		alive ++;
+		playerList.add(p4);
 		
-		if(num<4)
+		if(numOfPlayers<4)
 		{
 			p4.active = false;
 			alive--;
+			playerList.remove(p4);
 		}
-		if(num<3)
+		if(numOfPlayers<3)
 		{
 			p3.active = false;
 			alive--;
+			playerList.remove(p3);
 		}
-		if(num<2)
+		if(numOfPlayers<2)
 		{
 			p2.active = false;
 			alive--;
+			playerList.remove(p2);
 		}
 		
 		this.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("ESCAPE"), "close");
